2195dbf96f7a8f3bf98d5ff739cf2fc8e7c71f5e
davidemarchini <marchini73@gmail.com>
2024-05-02 17:32:05 +0200

    Creazione del Plan Seeder

--- database/seeders/PlanSeeder.php
+++ database/seeders/PlanSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Plan;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,18 @@ class PlanSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Recupero dati dei pacchetti di promozione
+
+        $plans= config("plans");
+
+        // Per ogni promozione creiamo una nuova istanza della classe Plan assegnando i dati ai relativi attributi e salvando la promozione nella tabella
+
+        foreach($plans as $plan){
+            $new_plan= new Plan();
+            $new_plan->name=$plan["name"];
+            $new_plan->time=$plan["time"];
+            $new_plan->price=$plan["price"];
+            $new_plan->save();
+        }
     }
 }
