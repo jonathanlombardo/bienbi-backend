52ccd725c6773795012f54430110edbaedae0ab3
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-02 19:44:41 +0200

    feat: Appartment Seeder

--- app/Models/Appartment.php
+++ app/Models/Appartment.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Appartment extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  // setta uno slug unico
+  public function setSlug()
+  {
+    $title = $this->title;
+    $slugs = Appartment::where('id', 'IS NOT', $this->id)->get()->pluck('slug')->toArray();
+    $baseSlug = Str::slug($title);
+    $newSlug = $baseSlug;
+    $counter = 0;
+
+    while (in_array($newSlug, $slugs)) {
+      $counter++;
+      $newSlug = $baseSlug . "-" . $counter;
+    }
+
+    $this->slug = $baseSlug;
+
+  }
 }
