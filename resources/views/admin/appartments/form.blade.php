f90eb607a8263ceaaa6e0c7a7bbd7b6b6a4f65c9
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-03 17:54:41 +0200

    add axios request

--- resources/views/admin/appartments/form.blade.php
+++ resources/views/admin/appartments/form.blade.php
@@ -13,6 +13,9 @@
       @csrf
       <div class="col-6">
         <div id="tomtom-searchbox"></div>
+        <input type="text" name="address" id="address" class="d-none">
+        <input type="number" name="lat" id="lat" class="d-none">
+        <input type="number" name="lng" id="lng" class="d-none">
       </div>
       <div class="col-6">
         <button class="btn btn-primary">Go</button>
@@ -21,6 +24,7 @@
 
   </div>
   <script>
+    // tomtom - searchbox
     const options = {
       searchOptions: {
         key: "GkJjTzfTAB01jy6W7VUViPfOdDf7dx9I",
@@ -30,16 +34,28 @@
       autocompleteOptions: {
         key: "GkJjTzfTAB01jy6W7VUViPfOdDf7dx9I",
         language: "it-IT",
-        countrySet: "IT"
       },
-      countrySet: "IT"
     }
     const ttSearchBox = new tt.plugins.SearchBox(tt.services, options)
     const searchBoxHTML = ttSearchBox.getSearchBoxHTML()
     const searchBoxEl = document.querySelector('#tomtom-searchbox');
     searchBoxEl.append(searchBoxHTML)
+
+    let clock;
+
     const searchBoxInput = document.querySelector('input.tt-search-box-input');
-    searchBoxInput.setAttribute('name', 'tt')
+    searchBoxInput.addEventListener('input', function() {
+      clearTimeout(clock);
+
+      clock = setTimeout(() => {
+        const query = searchBoxInput.value;
+        axios.get(`https://api.tomtom.com/search/2/search/${query}.json?key=GkJjTzfTAB01jy6W7VUViPfOdDf7dx9I&typeahead=true&countrySet=IT&language=it-IT&idxSet=Geo,PAD,Addr,Str,XStr`).then((res) => {
+          console.log(res.data);
+        })
+      }, 500);
+
+    })
+    // searchBoxInput.setAttribute('name', 'tt')
   </script>
 @endsection
 
