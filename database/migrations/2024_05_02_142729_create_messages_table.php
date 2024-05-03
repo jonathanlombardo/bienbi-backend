bde9089ae75f1c1e3eff533461443e278ba40fb0
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-03 11:25:54 +0200

    feat add foreing key

--- database/migrations/2024_05_02_142729_create_messages_table.php
+++ database/migrations/2024_05_02_142729_create_messages_table.php
@@ -4,33 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('messages', function (Blueprint $table) {
-            $table->id();
-            $table->text('body');
-            $table->string('mail');
-            $table->string('first_name', 40);
-            $table->string('last_name', 40);
-            // $table->foreignId('appartment_id')->constrained()->onDelete('cascade');
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('messages', function (Blueprint $table) {
+      $table->id();
+      $table->text('body');
+      $table->string('mail');
+      $table->string('first_name', 40);
+      $table->string('last_name', 40);
+      $table->foreignId('appartment_id')->constrained()->onDelete('cascade');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('messages');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('messages');
+  }
 };
