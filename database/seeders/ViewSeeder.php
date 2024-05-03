dcd10f60911bbf1efbf05e633c9129df79cecf30
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-03 10:16:43 +0200

    fix: view seeder

--- database/seeders/ViewSeeder.php
+++ database/seeders/ViewSeeder.php
@@ -6,26 +6,26 @@ use App\Models\View;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+
 class ViewSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run(Faker $faker)
-    {
-        for($i = 0; $i < 500; $i++){
-            
-          // $property = new Property();
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run(Faker $faker)
+  {
+    for ($i = 0; $i < 500; $i++) {
 
-          //creo nuovo oggetto
-          $newView = new View();
-          // $newView->property_id = $property->id;
-          $date = $faker->dateTimeBetween("-1 year", "now");
+      //creo nuovo oggetto
+      $newView = new View();
+      // $newView->property_id = $property->id;
+      $newView->date = $faker->dateTimeBetween("-1 year", "now");
+      $newView->ip_address = $faker->ipv4();
 
-          //salvo
-          $newView->save();
-        }
+      //salvo
+      $newView->save();
     }
+  }
 }
