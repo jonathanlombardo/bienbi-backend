9d518a446c8aacdd310212eef3162db8f67bcc00
sisdream <sissipascale2@gmail.com>
2024-05-03 12:08:30 +0200

    feat: reate routes views

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\View;
 use Illuminate\Http\Request;
 
 class ViewController extends Controller
 {
-    //
+     /**
+     * Display a listing of the resource.
+     *
+     * 
+     */
+    public function index()
+    {
+        
+        $services = View::paginate(10);
+        
+        return view('admin.view.index', compact('views'));
+    }
 }
