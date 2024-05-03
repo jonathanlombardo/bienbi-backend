24a3b11946ffe890062e85b46cfe72daaf73da4d
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-03 11:41:52 +0200

    feat: edit seeders

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -2,25 +2,31 @@
 
 namespace Database\Seeders;
 
+use App\Models\Appartment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Message;
 
 class MessageSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Message::create([
-            'body' => 'Contenuto del messaggio...',
-            'mail' => 'cicciopasticcio@example.com',
-            'first_name' => 'Nome',
-            'last_name' => 'Cognome',
-            // 'appartment_id' => 1
-        ]);
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $appartments = Appartment::all()->pluck('id')->toArray();
+    foreach ($appartments as $appartment) {
     }
+    for ($i = 0; $i < 4; $i++) {
+      Message::create([
+        'body' => 'Contenuto per appartamento ' . $appartment,
+        'mail' => 'cicciopasticcio@example.com',
+        'first_name' => 'Nome',
+        'last_name' => 'Cognome',
+        'appartment_id' => $appartment
+      ]);
+    }
+  }
 }
--- database/seeders/ViewSeeder.php
+++ database/seeders/ViewSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Appartment;
 use App\Models\View;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,13 +17,14 @@ class ViewSeeder extends Seeder
    */
   public function run(Faker $faker)
   {
+    $appartments = Appartment::all()->pluck('id')->toArray();
     for ($i = 0; $i < 500; $i++) {
 
       //creo nuovo oggetto
       $newView = new View();
-      // $newView->property_id = $property->id;
       $newView->date = $faker->dateTimeBetween("-1 year", "now");
       $newView->ip_address = $faker->ipv4();
+      $newView->appartment_id = array_rand($appartments);
 
       //salvo
       $newView->save();
