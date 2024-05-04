c111d04b54c2aa3742247dadc741af4bdaeac258
sisdream <sissipascale2@gmail.com>
2024-05-04 19:32:52 +0200

    fix: formattazione index

--- resources/views/admin/appartments/index.blade.php
+++ resources/views/admin/appartments/index.blade.php
@@ -1,54 +1,34 @@
 @extends('layouts.main')
 
 @section('maincontent')
-
-        <div class="container">
-          <h1 class="my-3">I Tuoi Appartamenti</h1>
-            <div class="row g-3">
-              @forelse($appartments as $appartment)
-
-              {{-- link per la show degli appartamenti --}}
-
-              <div class="col-4 mb-5 my-col">
-
-                <a href="{{route('admin.appartments.show', $appartment)}}" class="my-card-link">
-
-
-                  <div class="my-card">
-
-                    <div class="my-card-header p-3">
-                      <div class="image-container ">
-                        <img src="{{asset($appartment->image)}}" alt="">
-                      </div>
-
-                    </div>
-
-                    <div class="my-card-body p-3">
-
-                      <div class="title mt-3">
-                        <span class="title">{{$appartment->title}}</span>
-                      </div>
-                      <div class="messages">
-                        <a class="nav-link nav_link" href="{{route('admin.messages.index',['appartment_slug' => $appartment->slug])}}">Vedi messaggi</a>
-                      </div>
-
-                    </div>
-
-                  </div>
-                </a>
-
-
+  <div class="container">
+    <h1 class="my-3">I Tuoi Appartamenti</h1>
+    <div class="row g-3">
+      @forelse($appartments as $appartment)
+        {{-- link per la show degli appartamenti --}}
+        <div class="col-4 mb-5 my-col">
+          <a href="{{route('admin.appartments.show', $appartment)}}" class="my-card-link">
+            <div class="my-card">
+              <div class="my-card-header p-3">
+                <div class="image-container ">
+                  <img src="{{asset($appartment->image)}}" alt="">
+                </div>
               </div>
-                
-
-
-                
-                
-              @empty
-              @endforelse
+              <div class="my-card-body p-3">
+                <div class="title mt-3">
+                  <span class="title">{{$appartment->title}}</span>
+                </div>
+                <div class="messages">
+                  <a class="nav-link nav_link" href="{{route('admin.messages.index',['appartment_slug' => $appartment->slug])}}">Vedi messaggi</a>
+                </div>
+              </div>
             </div>
-            
+          </a>
         </div>
+        @empty
+      @endforelse
+    </div>
+  </div>
 @endsection
 
 <style lang="scss" scoped>
