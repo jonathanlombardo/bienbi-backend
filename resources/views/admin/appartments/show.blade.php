da1185a00ba6dcfa5797432446d153a537fc4eb8
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-05 15:35:59 +0200

    fix appartment show

--- resources/views/admin/appartments/show.blade.php
+++ resources/views/admin/appartments/show.blade.php
@@ -1,92 +1,76 @@
 @extends('layouts.main')
 
 @section('maincontent')
-  <div class="container">
+  <div class="container appartment-wrapper">
     <h1 class="my-3">{{ $appartment->title }}</h1>
-    <div class="row g-3">
-
-      <div class="my-card d-flex">
-        <div class="my-card-header p-3">
-          <div class="image-container ">
-            <img src="{{ $appartment->imgUrl }}" alt="">
-          </div>
-
-        </div>
-
-        <div class="my-card-body p-3">
-
-          <ul>
-            <li>
-              <strong>Stanze</strong>
-              <div>{{ $appartment->rooms > 1 ? $appartment->rooms : 'Una' }} {{ $appartment->rooms > 1 ? ' stanze' : ' stanza' }}</div>
-            </li>
-            <li>
-              <strong>Letti</strong>
-              <div>{{ $appartment->beds > 1 ? $appartment->beds : 'Un' }} {{ $appartment->beds > 1 ? ' letti' : ' letto' }}</div>
-            </li>
-            <li>
-              <strong>Bagni</strong>
-              <div>{{ $appartment->bathrooms > 1 ? $appartment->bathrooms : 'Un' }} {{ $appartment->bathrooms > 1 ? ' bagni' : ' bagno' }}</div>
-            </li>
-            <li>
-              <strong>Metri Quadri</strong>
-              <div>{{ $appartment->square_meters }} m2</div>
-            </li>
-            <li>
-              <strong>Indirizzo</strong>
-              <div>{{ $appartment->address }}</div>
-            </li>
-            <li>
-              <strong>{{ $appartment->published ? 'Gli utenti possono vedere questo appartamento' : 'Gli utenti non possono vedere questo appartamento' }}</strong>
-            </li>
-            <li class="d-flex align-items-end">
-
-              <div class="messages">
-                <a class="nav-link nav_link" href="{{ route('admin.messages.index', ['appartment_slug' => $appartment->slug]) }}">Vedi messaggi</a>
-              </div>
-
-            </li>
-          </ul>
-
+    <div class="row g-5">
+      <div class="col-12 col-lg-6">
+        <div class="image-container ">
+          <img src="{{ $appartment->imgUrl }}" alt="">
         </div>
       </div>
-
+      <div class="col-12 col-lg-6">
+        <ul class="h-100 p-0">
+          <li>
+            <strong>Stanze</strong>
+            <span>{{ $appartment->rooms > 1 ? $appartment->rooms : 'Una' }} {{ $appartment->rooms > 1 ? ' stanze' : ' stanza' }}</span>
+          </li>
+          <li>
+            <strong>Letti</strong>
+            <span>{{ $appartment->beds > 1 ? $appartment->beds : 'Un' }} {{ $appartment->beds > 1 ? ' letti' : ' letto' }}</span>
+          </li>
+          <li>
+            <strong>Bagni</strong>
+            <span>{{ $appartment->bathrooms > 1 ? $appartment->bathrooms : 'Un' }} {{ $appartment->bathrooms > 1 ? ' bagni' : ' bagno' }}</span>
+          </li>
+          <li>
+            <strong>Metri Quadri</strong>
+            <span>{{ $appartment->square_meters }} m2</span>
+          </li>
+          <li>
+            <strong>Indirizzo</strong>
+            <span>{{ $appartment->address }}</span>
+          </li>
+          <li>
+            <strong>{{ $appartment->published ? 'Gli utenti possono vedere questo appartamento' : 'Gli utenti non possono vedere questo appartamento' }}</strong>
+          </li>
+          <li class="row appartment-services">
+            <strong>Servizi offerti</strong>
+            @foreach ($appartment->services as $service)
+              <div class="col-6 d-flex align-items-center">
+                <i class="{{ $service->faIconClass }}"></i>
+                <div>{{ $service->label }}</div>
+              </div>
+            @endforeach
+          </li>
+          <li class="mt-auto">
+            <a class="nav-link nav_link" href="{{ route('admin.messages.index', ['appartment_slug' => $appartment->slug]) }}">Vedi messaggi</a>
+          </li>
+        </ul>
+      </div>
     </div>
-
   </div>
 @endsection
 
-<style lang="scss" scoped>
-  .my-col {
-    padding: 20px;
-    border-radius: 10px;
+@push('assets')
+  <style lang="scss">
+    .appartment-wrapper {
+      ul {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+      }
 
-  }
+      .image-container {
+        border-radius: 10px;
+        overflow: hidden;
+      }
 
-  .my-card-link {
-    color: black;
-    text-decoration: none;
-  }
-
-
-  .my-card {
-    .title {
-      font-size: 1rem,
-        font-weight: 500,
+      .appartment-services {
+        i {
+          width: 30px;
+        }
+      }
     }
-
-    li {
-      margin: 10px 0;
-    }
-
-    li:last-child {
-      height: 150px;
-    }
-  }
-
-
-  .image-container {
-    border-radius: 10px;
-    overflow: hidden;
-  }
-</style>
+  </style>
+@endpush
