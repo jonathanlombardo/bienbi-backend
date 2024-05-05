d72655ee5d2c76b7caab109d759d4596399fc3b4
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-05 19:09:52 +0200

    init appartment form validation

--- resources/views/admin/appartments/form.blade.php
+++ resources/views/admin/appartments/form.blade.php
@@ -11,54 +11,79 @@
 @section('maincontent')
   <div class="container">
     <h1>Crea un appartamento</h1>
-    <form action="{{ route('admin.appartments.store') }}" method="POST" class="row g-4" id="edit-form">
+
+    @if ($errors->any())
+      <div class="error-alert alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Check and correct fields on error!</strong>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    @endif
+
+    <form action="{{ $appartment->id ? route('admin.appartments.update', $appartment) : route('admin.appartments.store') }}" method="POST" class="row g-4" id="edit-form">
       @csrf
+      @method($appartment->id ? 'PATCH' : 'POST')
+
       <div class="col-12">
         @include('layouts.partials.tomtomSearchbox')
       </div>
 
       <div class="col-12">
         <label for="image" class="form-label">Immagine</label>
-        <input type="file" class="form-control custom-input image-preview-input" id="image" name="image" value="{{ $appartment->id ? $appartment->image : '' }}">
+        <input required type="file" class="@error('image') is-invalid @enderror form-control custom-input image-preview-input" id="image" name="image" value="{{ $appartment->id ? ($errors->any() ? old('image') : $appartment->image) : old('image') }}">
         <div class="mt-3">
           <img class="image-preview" src="{{ $appartment->imgUrl }}" alt="" class="proj-img">
           <button class="reset-img btn btn-link" type="button">Reset immagine</button>
         </div>
-        <div class="invalid-feedback input-feedback img-fb">errore</div>
+        @error('image')
+          <div class="invalid-feedback input-feedback">{{ $message }}</div>
+        @enderror
       </div>
 
       <div class="col-12">
         <label for="title" class="form-label">Titolo descrittivo</label>
-        <input type="text" class="form-control custom-input" id="title" name="title" placeholder="Appartamento da mario, villetta con terrazzo" value="{{ $appartment->id ? $appartment->title : '' }}">
-        <div class="invalid-feedback input-feedback">errore</div>
+        <input required type="text" class="@error('title') is-invalid @enderror form-control custom-input" id="title" name="title" placeholder="Appartamento da mario, villetta con terrazzo" value="{{ $appartment->id ? ($errors->any() ? old('title') : $appartment->title) : old('title') }}">
+        @error('title')
+          <div class="invalid-feedback input-feedback">{{ $message }}</div>
+        @enderror
       </div>
       <div class="col-12">
         <label for="rooms" class="form-label">Numero di stanze</label>
-        <input type="number" class="form-control custom-input" id="rooms" name="rooms" placeholder="2" value="{{ $appartment->id ? $appartment->rooms : '' }}">
-        <div class="invalid-feedback input-feedback">errore</div>
+        <input required type="number" class="@error('rooms') is-invalid @enderror form-control custom-input" id="rooms" name="rooms" placeholder="2" value="{{ $appartment->id ? ($errors->any() ? old('rooms') : $appartment->rooms) : old('rooms') }}">
+        @error('rooms')
+          <div class="invalid-feedback input-feedback">{{ $message }}</div>
+        @enderror
       </div>
       <div class="col-12">
         <label for="beds" class="form-label">Numero di posti letto</label>
-        <input type="number" class="form-control custom-input" id="beds" name="beds" placeholder="2" value="{{ $appartment->id ? $appartment->beds : '' }}">
-        <div class="invalid-feedback input-feedback">errore</div>
+        <input required type="number" class="@error('beds') is-invalid @enderror form-control custom-input" id="beds" name="beds" placeholder="2" value="{{ $appartment->id ? ($errors->any() ? old('beds') : $appartment->beds) : old('beds') }}">
+        @error('beds')
+          <div class="invalid-feedback input-feedback">{{ $message }}</div>
+        @enderror
       </div>
       <div class="col-12">
         <label for="bathrooms" class="form-label">Numero di bagni</label>
-        <input type="number" class="form-control custom-input" id="bathrooms" name="bathrooms" placeholder="2" value="{{ $appartment->id ? $appartment->bathrooms : '' }}">
-        <div class="invalid-feedback input-feedback">errore</div>
+        <input required type="number" class="@error('bathrooms') is-invalid @enderror form-control custom-input" id="bathrooms" name="bathrooms" placeholder="2" value="{{ $appartment->id ? ($errors->any() ? old('bathrooms') : $appartment->bathrooms) : old('bathrooms') }}">
+        @error('bathrooms')
+          <div class="invalid-feedback input-feedback">{{ $message }}</div>
+        @enderror
       </div>
       <div class="col-12">
         <label for="square_meters" class="form-label">Superfice (metri quadri)</label>
-        <input type="number" class="form-control custom-input" id="square_meters" name="square_meters" placeholder="2" value="{{ $appartment->id ? $appartment->square_meters : '' }}">
-        <div class="invalid-feedback input-feedback">errore</div>
+        <input required type="number" class="@error('square_meters') is-invalid @enderror form-control custom-input" id="square_meters" name="square_meters" placeholder="2" value="{{ $appartment->id ? ($errors->any() ? old('square_meters') : $appartment->square_meters) : old('square_meters') }}">
+        @error('square_meters')
+          <div class="invalid-feedback input-feedback">{{ $message }}</div>
+        @enderror
       </div>
 
-      <div class="col-12 row g-3">
+      <div class="col-12 row g-4">
         <div class="col-12">Servizi offerti</div>
         @foreach ($services as $service)
           <div class="col-12 col-md-6 col-xl-4">
-            <input type="checkbox" class="form-check-input" name="services[]" id="service{{ $service->id }}" value="{{ $service->id }}" {{ $appartment->id ? (in_array($service->id, $appartmentServices) ? 'checked' : '') : '' }}>
+            <input type="checkbox" class="@error('services') is-invalid @enderror form-check-input" name="services[]" id="service{{ $service->id }}" value="{{ $service->id }}" {{ in_array($service->id, old('services', $appartment->id ? $appartmentServices : [])) ? 'checked' : '' }}>
             <label class="form-check-label ms-2" for="service{{ $service->id }}">{{ $service->label }}</label>
+            @error('services')
+              <div class="invalid-feedback input-feedback">{{ $message }}</div>
+            @enderror
           </div>
         @endforeach
       </div>
@@ -66,9 +91,12 @@
       <div class="col-12">
         <span>Pubblica:</span>
         <label class="switch">
-          <input type="checkbox" name="published">
+          <input type="checkbox" name="published" {{ old('published', $appartment->id ? $appartment->published : null) ? 'checked' : '' }}>
           <span class="slider round"></span>
         </label>
+        @error('published')
+          <div class="invalid-feedback input-feedback">{{ $message }}</div>
+        @enderror
       </div>
       <div class="col-12">
         <button type="submit" class="btn btn-primary">{{ $appartment->id ? 'Modifica Appartamento' : 'Crea Appartamento' }}</button>
@@ -81,9 +109,15 @@
 @push('scripts')
   <script>
     const inputs = document.querySelectorAll('input');
+    const errorAlertEl = document.querySelector('.error-alert');
 
     inputs.forEach((input) => {
       input.addEventListener("input", function() {
+        this.classList.remove('is-invalid')
+        if (errorAlertEl) errorAlertEl.classList.add('d-none')
+      })
+
+      input.addEventListener("change", function() {
         this.classList.remove('is-invalid')
         if (errorAlertEl) errorAlertEl.classList.add('d-none')
       })
