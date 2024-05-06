52d1198cd5d585c38e6461693916842e3a95c6a2
sisdream <sissipascale2@gmail.com>
2024-05-06 12:20:09 +0200

    fix: color

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -2,6 +2,9 @@
 
 @section('maincontent')
 <div class="container">
+  <div>
+    <h1 class="my-3">MESSAGGI</h1>
+  </div>
   <div class="row mt-5">
     <div class="col">
       <div class="my-card">
@@ -10,7 +13,6 @@
             <div class="name">{{$message->first_name}} {{$message->last_name}}</div>
             <div class="mail">{{$message->mail}}</div>
           </div>
-
           <div class="dropdown">
             <button class="btn option-btn align-middle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
               <i class="fa-solid fa-ellipsis"></i>
@@ -18,46 +20,48 @@
             <ul class="dropdown-menu p-1 ">
               <li class="text-center ">
                 <!-- Button trigger modal -->
-                <button type="button" class="btn delete-btn " data-bs-toggle="modal" data-bs-target="#delete-{{$message->id}}">
+                <button type="button" class="btn delete-btn" data-bs-toggle="modal" data-bs-target="#delete-{{$message->id}}">
                   Elimina Messaggio
-                </button>
+                </button>    
               </li>
             </ul>
           </div>
+
           {{-- modal --}}
                         
           <div class="modal fade" id="delete-{{$message->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
               <div class="modal-content">
                 <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminare da {{ $message->first_name }} {{ $message->last_name}}?</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminare da {{ $message->first_name }} {{ $message->last_name}}?</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                            Se elimini questo messaggio non potrai più recuperarlo.
-                </div>
-                <div class="modal-footer">
-                                
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
 
-                            <form action="{{ route('admin.messages.destroy', $message)}}" method='POST'>
-                            @csrf
-                            @method('DELETE')
-                              <button class='btn btn-danger'>Elimina</button>
-                            </form>
-                                
+                <div class="modal-body">
+                  Se elimini questo messaggio non potrai più recuperarlo.
+                </div>
+
+                <div class="modal-footer">   
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                  <form action="{{ route('admin.messages.destroy', $message)}}" method='POST'>
+                    @csrf
+                    @method('DELETE')
+                    <button class='btn btn-danger'>Elimina</button>
+                  </form>         
+                </div>
+                </div>
                 </div>
               </div>
             </div>
+                
+            <div class="my-card-body d-flex justify-content-between">
+              <div class="message">{{$message->body}}</div>
+              <div class="created">{{$message->created_at}}</div>
+            </div>
           </div>
         </div>
-
-        <div class="my-card-body d-flex justify-content-between">
-          <div class="message">{{$message->body}}</div>
-          <div class="created">{{$message->created_at}}</div>
-        </div>
       </div>
-      </div>
+    </div>
     </div>
   </div>
 </div>
@@ -67,9 +71,10 @@
 
   .my-card{
     padding: 10px 20px;
-    border: 0.1px solid rgb(205, 205, 205);
+    border-radius: 10px;    
+    border: 0.1px solid rgb(236, 236, 236);
     background: #f3c665;
-
+    
     .name{
       font-size: 1rem;
     }
@@ -84,7 +89,6 @@
     .delete-btn{
       padding: 0;
       border: none;
-      background: #f35138;
     }
 
     ul{
@@ -95,4 +99,5 @@
       font-size: 0.7rem;
     }
   }
+
 </style>
