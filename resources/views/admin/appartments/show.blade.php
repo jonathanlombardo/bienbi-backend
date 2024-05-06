cf323eba1e087a6ec4578ee63e930b3bf6195c53
yusef02 <youssef.bouziane2402@gmail.com>
2024-05-06 16:30:49 +0200

    fix show appartment

--- resources/views/admin/appartments/show.blade.php
+++ resources/views/admin/appartments/show.blade.php
@@ -1,81 +1,90 @@
 @extends('layouts.main')
 
 @section('maincontent')
-  <div class="container appartment-wrapper">
-    <h1 class="my-3">{{ $appartment->title }}</h1>
-    <div class="row d-flex flex-column g-5">
-      <div class="col-line col-12 col-lg-6">
-        <div class="image-container ">
-          <img src="{{ $appartment->imgUrl }}" alt="">
-        </div>
-      </div>
-      <div class="col-line col-12 col-lg-6">
-        <ul class="h-100 p-0">
-          <li>
-            <strong>Stanze</strong>
-            <span>{{ $appartment->rooms > 1 ? $appartment->rooms : 'Una' }} {{ $appartment->rooms > 1 ? ' stanze' : ' stanza' }}</span>
-          </li>
-          <li>
-            <strong>Letti</strong>
-            <span>{{ $appartment->beds > 1 ? $appartment->beds : 'Un' }} {{ $appartment->beds > 1 ? ' letti' : ' letto' }}</span>
-          </li>
-          <li>
-            <strong>Bagni</strong>
-            <span>{{ $appartment->bathrooms > 1 ? $appartment->bathrooms : 'Un' }} {{ $appartment->bathrooms > 1 ? ' bagni' : ' bagno' }}</span>
-          </li>
-          <li>
-            <strong>Metri Quadri</strong>
-            <span>{{ $appartment->square_meters }} m2</span>
-          </li>
-          <li>
-            <strong>Indirizzo</strong>
-            <span>{{ $appartment->address }}</span>
-          </li>
-          <li>
-            <strong>{{ $appartment->published ? 'Gli utenti possono vedere questo appartamento' : 'Gli utenti non possono vedere questo appartamento' }}</strong>
-          </li>
-          <li class="row appartment-services">
-            <strong>Servizi offerti</strong>
-            @foreach ($appartment->services as $service)
-              <div class="col-6 d-flex align-items-center">
-                <i class="{{ $service->faIconClass }}"></i>
-                <div>{{ $service->label }}</div>
-              </div>
-            @endforeach
-          </li>
-          <li class="m-2">
-            <a class="nav-link page-link" href="{{ route('admin.messages.appartment.index', ['appartment_slug' => $appartment->slug]) }}">Vedi messaggi</a>
-          </li>
-        </ul>
+<div class="container appartment-wrapper">
+  <h1 class="my-3">{{ $appartment->title }}</h1>
+  <div class="row d-flex flex-column g-5">
+    <div class="col-line col-12 col-lg-6">
+      <div class="image-container ">
+        <img src="{{ $appartment->imgUrl }}" alt="">
       </div>
     </div>
+    <div class="col-line col-12 col-lg-6">
+      <ul class="h-100 p-0">
+        <li>
+          <img class='img-services d-inline-block' src="{{asset('/storage/stanze.png')}}">
+          <strong>Stanze</strong>
+          <span>{{ $appartment->rooms > 1 ? $appartment->rooms : 'Una' }} {{ $appartment->rooms > 1 ? ' stanze' : ' stanza' }}</span>
+        </li>
+        <li>
+          <img class='img-services d-inline-block' src="{{asset('/storage/letti.png')}}">
+          <strong>Letti</strong>
+          <span>{{ $appartment->beds > 1 ? $appartment->beds : 'Un' }} {{ $appartment->beds > 1 ? ' letti' : ' letto' }}</span>
+        </li>
+        <li>
+          <img class='img-services d-inline-block' src="{{asset('/storage/bagni.png')}}">
+          <strong>Bagni</strong>
+          <span>{{ $appartment->bathrooms > 1 ? $appartment->bathrooms : 'Un' }} {{ $appartment->bathrooms > 1 ? ' bagni' : ' bagno' }}</span>
+        </li>
+        <li>
+          <img class='img-services d-inline-block' src="{{asset('/storage/mq.png')}}">
+          <strong>Metri Quadri</strong>
+          <span>{{ $appartment->square_meters }} m2</span>
+        </li>
+        <li>
+          <strong>Indirizzo</strong>
+          <span>{{ $appartment->address }}</span>
+        </li>
+        <li>
+          <strong>{{ $appartment->published ? 'Gli utenti possono vedere questo appartamento' : 'Gli utenti non possono vedere questo appartamento' }}</strong>
+        </li>
+        <li class="row appartment-services">
+          <strong>Servizi offerti</strong>
+          @foreach ($appartment->services as $service)
+          <div class="col-6 d-flex align-items-center">
+            <i class="{{ $service->faIconClass }}"></i>
+            <div>{{ $service->label }}</div>
+          </div>
+          @endforeach
+        </li>
+        <li class="m-2">
+          <a class="nav-link page-link" href="{{ route('admin.messages.appartment.index', ['appartment_slug' => $appartment->slug]) }}">Vedi messaggi</a>
+        </li>
+      </ul>
+    </div>
   </div>
+</div>
 @endsection
 
 @push('assets')
-  <style lang="scss">
-    .col-line{
-      border-bottom: 1px solid gray;
-      height: 400px;
+<style lang="scss">
+  .col-line {
+    border-bottom: 1px solid gray;
+    height: 400px;
+  }
+
+  .img-services {
+    width: 40px;
+    aspect-ratio: 1;
+  }
+
+  .appartment-wrapper {
+    ul {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
     }
 
-    .appartment-wrapper {
-      ul {
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
-      }
+    .image-container {
+      border-radius: 10px;
+      overflow: hidden;
+    }
 
-      .image-container {
-        border-radius: 10px;
-        overflow: hidden;
-      }
-
-      .appartment-services {
-        i {
-          width: 30px;
-        }
+    .appartment-services {
+      i {
+        width: 30px;
       }
     }
-  </style>
+  }
+</style>
 @endpush
