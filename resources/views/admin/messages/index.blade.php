ce509b3b368c1a076933668a692e77422571644e
sisdream <sissipascale2@gmail.com>
2024-05-06 14:48:36 +0200

    fix: hover card message

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -1,22 +1,17 @@
 @extends('layouts.main')
 
 @section('maincontent')
+  <div class="container">
+    <div>
+      <h2 class="title my-3">MESSAGGI</h2>
+    </div>
 
+    <div class="row mt-5">
+      <div class="col">
         <div class="container">
-          <div>
-            <h1 class="my-3">MESSAGGI</h1>
-          </div>
-
-            <div class="row mt-5">
-                <div class="col">
-
-
-                  <div class="container">
-                    <div class="row g-2">
-
-                      @forelse($messages as $message)
-
-                      <div class="col-6 position-relative">
+          <div class="row g-2">
+            @forelse($messages as $message)
+              <div class="col-6 position-relative">
 
                         <a href="{{route('admin.messages.show', $message)}}" class="my-card-link">
 
@@ -83,90 +78,23 @@
                           </div>
                         </div>
 
-                      </div>
-
-                      @empty
-                      @endforelse
-
-                    </div>
-                  </div>
-
-                    {{-- @forelse($appartments as $appartment)
-                
-                    <table class="table">
-                        <thead>
-                          <tr>
-                            <th class="text-center" scope="col">Nome</th>
-                            <th class="text-center" scope="col">Cognome</th>
-                            <th class="text-center" scope="col">Mail</th>
-                            <th class="text-center" scope="col">Data di invio</th>
-                            <th class="text-center" scope="col">Appartamento</th>
-                            <th class="text-center" scope="col"></th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                            @forelse($messages as $message)
-
-                              
-                            <tr class="align-middle">
-                              <td class="text-center">{{$message->first_name}}</td>
-                              <td class="text-center">{{$message->last_name}}</td>
-                              <td class="text-center">{{$message->mail}}</td>
-                              <td class="text-center">{{$message->created_at}}</td>
-                              <td class="text-center">{{$message->appartment_id}}</td>
-                              <td class="text-end">
-                                <div class="btn"><a class="btn" href="{{route('admin.messages.show', $message)}}"> <i class="fa-solid fa-eye text-warning"></i> </a></div>
-                                <!-- Button trigger modal -->
-                                <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#delete-{{$message->id}}">
-                                  <i class="fa-solid fa-trash text-danger"></i>
-                                </button>
-
-                                {{-- modal --}}
-                                
-                                {{-- <div class="modal fade" id="delete-{{$message->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                  <div class="modal-dialog modal-dialog-centered">
-                                    <div class="modal-content">
-                                      <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminare da {{ $message->first_name }} {{ $message->last_name}}?</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                      </div>
-                                      <div class="modal-body">
-                                        Se elimini questo messaggio non potrai più recuperarlo.
-                                      </div>
-                                      <div class="modal-footer">
-                                              
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-      
-                                        <form action="{{ route('admin.messages.destroy', $message)}}" method='POST'>
-                                        @csrf
-                                        @method('DELETE')
-                                          <button class='btn btn-danger'>Elimina</button>
-                                        </form>
-                                              
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-
-                              </td>
-
-                            </tr>
-                            
-                            @empty
-                            @endforelse
-                        </tbody>
-                      </table>
-                      
-                      {{$appartments->links()}}
-                    @empty
-                    @endforelse  --}}
-                </div>
-            </div>
-            
-        </div>
+              </div>
+            @empty
+            @endforelse
+          </div>
+        </div>       
+      </div>
+    </div>   
+  </div>
 @endsection
 
 <style lang="scss" scoped>
+
+   .title{
+    color: #e8620c;
+    font-size: 3rem;
+    font-weight: bold;
+  }
 
   .my-card-link{
     color: black;
@@ -177,7 +105,9 @@
     padding: 10px 20px;
     border: 0.1px solid rgb(255, 255, 255);
     position: relative;
-
+    opacity: 0.6;
+    background: #f3c665;
+    cursor: pointer;
 
     .name{
       font-size: 1rem;
@@ -196,7 +126,6 @@
     .delete-btn{
       padding: 0;
       border: none;
-
     }
 
     ul{
@@ -210,13 +139,11 @@
 
   .my-card:hover{
     border: 0.1px solid rgb(236, 236, 236);
-    border-radius: 10px;
+    opacity: 1;
   }
 
   .my-dropdown{
     right: 5%;
     top: 20%;
   }
-
-
 </style>
