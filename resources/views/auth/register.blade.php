9c8035289daf0ff1f52f1bfba67d97c624660703
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-05-07 16:42:02 +0200

    fix lenguage

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,19 +5,19 @@
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card">
-          <div class="card-header card_header_bg text-center">{{ __('Register') }}</div>
+          <div class="card-header card_header_bg text-center">{{ __('Registrazione') }}</div>
           <div class="container mt-4">
             <div class="row justify-content-center">
               <div class="col-md-8">
                 <div class="card">
-                  <div class="card-header">{{ __('Register') }}</div>
+                  <div class="card-header">{{ __('Registrazione') }}</div>
 
                   <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                       @csrf
 
                       <div class="mb-4 row">
-                        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                        <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
                         <div class="col-md-6">
                           <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -31,7 +31,7 @@
                       </div>
 
                       <div class="mb-4 row">
-                        <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Last name') }}</label>
+                        <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Cognome') }}</label>
 
                         <div class="col-md-6">
                           <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" required>
@@ -45,7 +45,7 @@
                       </div>
 
                       <div class="mb-4 row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                         <div class="col-md-6">
                           <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -74,7 +74,7 @@
 
                       <div class="mb-4 row mb-0">
                         <div class="mb-4 row">
-                          <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                          <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label>
 
                           <div class="col-md-6">
                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -82,7 +82,7 @@
                         </div>
 
                         <div class="mb-4 row">
-                          <label for="birthday" class="col-md-4 col-form-label text-md-right">{{ __('Date of birth') }}</label>
+                          <label for="birthday" class="col-md-4 col-form-label text-md-right">{{ __('Data di nascita') }}</label>
 
                           <div class="col-md-6">
                             <input id="birthday" type="date" class="form-control @error('birthday') is-invalid @enderror" name="birthday">
