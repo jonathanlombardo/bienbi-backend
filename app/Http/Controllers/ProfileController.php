671c14e8b51e78afaf0a936598d186fd4a825f21
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-08 11:29:05 +0200

    fix profile update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -26,7 +26,8 @@ class ProfileController extends Controller
    */
   public function update(ProfileUpdateRequest $request): RedirectResponse
   {
-    $request->user()->fill($request->validated());
+    $request->validated();
+    $request->user()->fill($request->all());
 
     if ($request->user()->isDirty('email')) {
       $request->user()->email_verified_at = null;
