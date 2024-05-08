6f8e62e0df304d94bff2cfe9479d2cac2a6fb78d
yusef02 <youssef.bouziane2402@gmail.com>
2024-05-08 15:14:44 +0200

    add params and generate url for request in appartmnet.filtered

--- app/Http/Controllers/Api/AppartmentController.php
+++ app/Http/Controllers/Api/AppartmentController.php
@@ -50,6 +50,39 @@ class AppartmentController extends Controller
 
   public function filtered(Request $request)
   {
-    //
+
+    // requpero dati della request (lat long raggio)
+    $data_req = $request->all();
+
+    // recupero tutti gli appartamenti
+    $appartments = Appartment::select('id', 'lat', 'long')->get();
+
+    // definisco i parametri per la richiesta api a tomtom per filtrare gli appartamenti
+    $params = [
+      'key' => 'GkJjTzfTAB01jy6W7VUViPfOdDf7dx9I',
+    ];
+
+    $geometry_list = [[
+      "type" => "CIRCLE",
+      "position" => "{$data_req['lat']}, {$data_req['long']}",
+      "radius" => $data_req['radius']
+    ]];
+
+    $geometry_list_json_string = json_encode($geometry_list);
+
+    $appartments_list = [[]];
+    foreach ($appartments as $appartment) {
+      $appartments_list[0][] = [
+        'poi' => ['id' => $appartment->id],
+        'position' => [
+          'lat' => $appartment->lat,
+          'long' => $appartment->long,
+        ]
+      ];
+    }
+    $appartments_list_json_string = json_encode($appartments_list);
+
+    // creo url concatenando i parametri
+    $url = 'https://api.tomtom.com/search/2/geometryFilter.json?key=' . $params['key'] . '&geometryList=' . $geometry_list_json_string . '&poiList=' . $appartments_list_json_string;
   }
 }
