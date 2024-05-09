676b380e1bb6b9329985daa115cbf7b7433ee8ef
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-09 11:15:45 +0200

    fix API show appartment - from id to slug

--- app/Http/Controllers/Api/AppartmentController.php
+++ app/Http/Controllers/Api/AppartmentController.php
@@ -46,9 +46,9 @@ class AppartmentController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function show($id)
+  public function show(string $slug)
   {
-    $appartment = Appartment::where('id', '=', $id)
+    $appartment = Appartment::where('slug', '=', $slug)
       ->where('published', '=', true)->first();
 
     if ($appartment)
