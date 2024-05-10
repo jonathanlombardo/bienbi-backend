39c2025bbe3411a8f23f8bdc8221eb867b60a87a
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-10 12:49:32 +0200

    fix tab subtitle profile

--- resources/views/auth/profile/edit.blade.php
+++ resources/views/auth/profile/edit.blade.php
@@ -1,37 +1,42 @@
 @extends('layouts.main')
+
+@section('title')
+  Profilo
+@endsection
+
 @section('maincontent')
-    <div class="container">
-        {{-- <h2 class="fs-4 text-secondary my-4">
+  <div class="container">
+    {{-- <h2 class="fs-4 text-secondary my-4">
             {{ __('I tuoi messaggi') }}
         </h2> --}}
 
-        <h2 class="fs-4 text-secondary my-4">
-            {{ __('Profilo') }}
-        </h2>
-        <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+    <h2 class="fs-4 text-secondary my-4">
+      {{ __('Profilo') }}
+    </h2>
+    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
 
-            @include('auth.profile.partials.update-profile-information-form')
+      @include('auth.profile.partials.update-profile-information-form')
 
-        </div>
-
-        <div class="card p-4 mb-4 bg-white shadow rounded-lg">
-
-
-            @include('auth.profile.partials.update-password-form')
-
-        </div>
-
-        <div class="card p-4 mb-4 bg-white shadow rounded-lg">
-
-
-            @include('auth.profile.partials.delete-user-form')
-
-        </div>
     </div>
+
+    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+
+
+      @include('auth.profile.partials.update-password-form')
+
+    </div>
+
+    <div class="card p-4 mb-4 bg-white shadow rounded-lg">
+
+
+      @include('auth.profile.partials.delete-user-form')
+
+    </div>
+  </div>
 @endsection
 
-@push("assets")
-<style>
+@push('assets')
+  <style>
     .my_btn {
       font-size: 0.9rem;
       background-color: #ffb30e;
@@ -52,5 +57,5 @@
       background-color: #f34e39;
       box-shadow: 2px 3px 12px #f34e39;
     }
-</style>
+  </style>
 @endpush
