934b74ebfaf35ac368b2625db308c5450426bfec
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-13 18:16:26 +0200

    fix typo sponsor-form

--- resources/views/admin/sponsor-form.blade.php
+++ resources/views/admin/sponsor-form.blade.php
@@ -18,7 +18,7 @@
                       <button class="p-0 accordion-button custom-acc-btn py-1 d-flex align-items-center collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePlan{{ $plan->id }}" aria-expanded="false" aria-controls="collapsePlan{{ $plan->id }}">
                         <h3 class="mb-0 input-wrapper d-flex align-items-center gap-3">
                           <input class="form-check-input fs-6" type="radio" id="plans{{ $plan->id }}" name="plans" class="btn btn-primary" value="{{ $plan->id }}">
-                          <div class="form-check-label">Paccheto {{ $plan->name }}</div>
+                          <div class="form-check-label">Pacchetto {{ $plan->name }}</div>
                         </h3>
                         <div class="details-wrapper d-flex align-items-center gap-3 ms-auto me-3">
                           <p class="mb-0"><i class="fa-regular fa-clock"></i> {{ $plan->getTime() }}</p>
