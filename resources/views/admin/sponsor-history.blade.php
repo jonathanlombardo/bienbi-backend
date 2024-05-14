6b2142151afb3ca08147591e1b4d146fa3a276a0
yusef02 <youssef.bouziane2402@gmail.com>
2024-05-14 15:24:48 +0200

    feat pivot relation data expire date and date issue

--- resources/views/admin/sponsor-history.blade.php
+++ resources/views/admin/sponsor-history.blade.php
@@ -12,10 +12,13 @@ Cronologia sponsorizzazioni per {{$appartment->title}}
     <div class="col-12">
       <div class="card">
 
-        nome appartamento: {{$appartment->title}} <br>
-        proprietario appartamento: {{$appartment->user->name}} {{$appartment->user->last_name}} <br>
-        titolo plan sponsor: {{$plan->name}} <br>
-        prezzo: {{$plan->price}} <br>
+        <span>Appartamento: <strong>{{$appartment->title}}</strong> </span><br>
+        <span>Proprietario: <strong>{{$appartment->user->name}} {{$appartment->user->last_name}}</strong> </span><br>
+        <span>Sponsorizzazione: <strong>{{$plan->name}}</strong> </span><br>
+        <span>Prezzo: <strong>{{$plan->price}}</strong> </span><br>
+        <span>Iniziato il: <strong>{{$plan->pivot->date_of_issue}}</strong> </span><br>
+        <span>Scadenza il: <strong>{{$plan->pivot->expired_at}}</strong> </span><br>
+
       </div>
     </div>
     @endforeach
