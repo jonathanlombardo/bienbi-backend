510ab6d2bf4c28f40b1658c16b1b3c177c5d66a1
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-15 23:55:11 +0200

    style and fix sponsor-history view

--- resources/views/admin/sponsor-history.blade.php
+++ resources/views/admin/sponsor-history.blade.php
@@ -1,108 +1,91 @@
 @extends('layouts.main')
 
 @section('title')
-Cronologia sponsorizzazioni per {{$appartment->title}}
+  Sponsorizzazioni
 @endsection
 
 @section('maincontent')
-<div class="container">
-  <h1 class="mb-3 text-center">Cronologia sponsorizzazioni per {{$appartment->title}}</h1>
+  <div class="container">
+    @if (!$sortedPlans->count())
+      <h1 class="mb-3 text-center">Nessuna sponsorizzazione{{ $appartment ? " per $appartment->title" : '' }}</h1>
+    @else
+      <h1 class="mb-3 text-center">Cronologia sponsorizzazioni per {{ $appartment ? $appartment->title : 'i tuoi appartamenti' }}</h1>
 
 
-  <div class="row g-3 my-4">
-    @foreach($appartment->plans as $plan)
-    <div class="col-4">
-      <div class="card text-center">
-
-        @if ($plan->name == 'Base')
-          <span class="card-header card-base">Sponsorizzazione: <strong>{{$plan->name}}</strong> </span>
-        @elseif ($plan->name == 'Medium')
-          <span class="card-header card-medium">Sponsorizzazione: <strong>{{$plan->name}}</strong> </span>
-        @elseif ($plan->name == 'Premium')
-          <span class="card-header card-premium">Sponsorizzazione: <strong>{{$plan->name}}</strong> </span>
-        @endif
-
-        @if ($plan->name == 'Base')
-          <div class="card-body d-flex flex-column gap-3 card-base-body">
-            <div>
-              <i class="fa-solid fa-star fs-5"></i>
+      <div class="row flex-column g-3 my-4">
+        @foreach ($sortedPlans as $plan)
+          <div class="col">
+            <div class="card text-center">
+              <div class="card-header card-{{ $plan->name }} d-flex justify-content-between">
+                <div>
+                  Sponsorizzazione: <strong>{{ $plan->name }}</strong>
+                  <span class="ms-2">
+                    <i class="fa-solid fa-star fs-5"></i>
+                    @if ($plan->name == 'Medium' || $plan->name == 'Premium')
+                      <i class="fa-solid fa-star fs-5"></i>
+                      @if ($plan->name == 'Premium')
+                        <i class="fa-solid fa-star fs-5"></i>
+                      @endif
+                    @endif
+                  </span>
+                </div>
+                <span>Appartamento: <strong>{{ $plan->appartment }}</strong> </span>
+              </div>
+              <div class="card-body d-flex flex-column gap-3 card-{{ $plan->name }}-body">
+                <span>Creato il: <strong>{{ $plan->createdDate }} alle ore {{ $plan->createdHour }}</strong> </span>
+                <div>
+                  <div class="mb-1">Periodo della sponsorizzazione:</div>
+                  <span>Dal <strong>{{ $plan->issueDate }} alle {{ $plan->issueHour }}</strong> al <strong>{{ $plan->expDate }} alle {{ $plan->expHour }}</strong></span>
+                </div>
+              </div>
+              <span class="card-footer card-{{ $plan->name }}">Prezzo: <strong>€ {{ $plan->price }}</strong> </span>
             </div>
-            <span>Appartamento: <strong>{{$appartment->title}}</strong> </span>
-            <span>Proprietario: <strong>{{$appartment->user->name}} {{$appartment->user->last_name}}</strong> </span>
-            <span>Iniziato il: <strong>{{$plan->pivot->date_of_issue}}</strong> </span>
-            <span>Scadenza il: <strong>{{$plan->pivot->expired_at}}</strong> </span>
           </div>
-        @elseif ($plan->name == 'Medium')
-          <div class="card-body d-flex flex-column gap-3 card-medium-body">
-            <div>
-              <i class="fa-solid fa-star fs-5"></i>
-              <i class="fa-solid fa-star fs-5"></i>
-            </div>
-            <span>Appartamento: <strong>{{$appartment->title}}</strong> </span>
-            <span>Proprietario: <strong>{{$appartment->user->name}} {{$appartment->user->last_name}}</strong> </span>
-            <span>Iniziato il: <strong>{{$plan->pivot->date_of_issue}}</strong> </span>
-            <span>Scadenza il: <strong>{{$plan->pivot->expired_at}}</strong> </span>
-          </div>
-        @elseif ($plan->name == 'Premium')
-          <div class="card-body d-flex flex-column gap-3 card-premium-body">
-            <div>
-              <i class="fa-solid fa-star fs-5"></i>
-              <i class="fa-solid fa-star fs-5"></i>
-              <i class="fa-solid fa-star fs-5"></i>
-            </div>
-            <span>Appartamento: <strong>{{$appartment->title}}</strong> </span>
-            <span>Proprietario: <strong>{{$appartment->user->name}} {{$appartment->user->last_name}}</strong> </span>
-            <span>Iniziato il: <strong>{{$plan->pivot->date_of_issue}}</strong> </span>
-            <span>Scadenza il: <strong>{{$plan->pivot->expired_at}}</strong> </span>
-          </div>
-        @endif
-
-        @if ($plan->price == '2.99')
-        <span class="card-footer card-base">Prezzo: <strong>{{$plan->price}}</strong> </span>
-        @elseif ($plan->price == '5.99')
-        <span class="card-footer card-medium">Prezzo: <strong>{{$plan->price}}</strong> </span>
-        @elseif ($plan->price == '9.99')
-        <span class="card-footer card-premium">Prezzo: <strong>{{$plan->price}}</strong> </span>
-        @endif
-
+        @endforeach
       </div>
+    @endif
+    <div class="text-center">
+      @if ($appartment)
+        <a href="#" class="my_btn">Torna ai dettagli</a>
+        <a href="#" class="my_btn">Sponsorizza appartamento</a>
+      @else
+        <a href="#" class="my_btn">Torna agli appartamenti</a>
+      @endif
     </div>
-    @endforeach
   </div>
-</div>
 @endsection
 
 
 @push('assets')
-<style>
-  .title {
-    color: #e8620c;
-    font-size: 3rem;
-    font-weight: bold;
-  }
+  <style>
+    .title {
+      color: #e8620c;
+      font-size: 3rem;
+      font-weight: bold;
+    }
 
-  .card-base {
-    background-color: #f3ddab !important;
-  }
+    .card-Base {
+      background-color: #f3ddab !important;
+    }
 
-  .card-base-body {
-    background-color: #f2e8d1 !important;
-  }
+    .card-Base-body {
+      background-color: #f2e8d1 !important;
+    }
 
-  .card-medium {
-    background-color: #ffb30e !important;
-  }
+    .card-Medium {
+      background-color: #ffb30e !important;
+    }
 
-  .card-medium-body {
-    background-color: #f6d99a !important;
-  }
+    .card-Medium-body {
+      background-color: #f6d99a !important;
+    }
 
-  .card-premium {
-    background-color: #f34e39 !important;
-  }
+    .card-Premium {
+      background-color: #f34e39 !important;
+    }
 
-  .card-premium-body {
-    background-color: #f7a398 !important;
-  }
-</style>
+    .card-Premium-body {
+      background-color: #f7a398 !important;
+    }
+  </style>
 @endpush
