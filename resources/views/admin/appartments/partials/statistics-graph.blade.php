1162e06a242e126785ba17d50c3e8ea90a8414a2
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-16 14:57:28 +0200

    feat getDateDiffObj function

--- resources/views/admin/appartments/partials/statistics-graph.blade.php
+++ resources/views/admin/appartments/partials/statistics-graph.blade.php
@@ -87,17 +87,19 @@
 
       // formatto le date in oggetti con proprietà data suddivise
       const views = [];
-      dateTimes.forEach(date => {
-        const dateView = new Date(Date.parse(date));
-        views.push({
-          hours: dateView.getHours(),
-          minutes: dateView.getMinutes(),
-          day: dateView.getDate(),
-          month: dateView.getMonth() + 1,
-          year: dateView.getFullYear(),
-          date: dateView.getFullYear() + '-' + (dateView.getMonth() < 10 ? '0' + (dateView.getMonth() + 1) : dateView.getMonth() + 1) + '-' + (dateView.getDate() < 10 ? '0' + dateView.getDate() : dateView.getDate()),
-          dateMonth: (dateView.getMonth() < 10 ? '0' + (dateView.getMonth() + 1) : dateView.getMonth() + 1) + '-' + dateView.getFullYear()
-        });
+      dateTimes.forEach((date) => {
+        // const dateView = new Date(Date.parse(date));
+        // const obj = {};
+        // obj.hours = dateView.getHours();
+        // obj.minutes = dateView.getMinutes();
+        // obj.day = dateView.getDate();
+        // obj.month = dateView.getMonth() + 1;
+        // obj.year = dateView.getFullYear();
+        // obj.date = obj.year + '-' + (obj.month < 10 ? '0' + obj.month : obj.month) + '-' + (obj.day < 10 ? '0' + obj.day : obj.day);
+        // obj.dateMonth = (obj.month < 10 ? '0' + obj.month : obj.month) + '-' + obj.year;
+        // obj.dateTime = obj.date + ' ' + obj.hours + ':' + obj.minutes;
+
+        views.push(parseDateObj(date));
       });
 
       return [totViews, views];
@@ -208,6 +210,98 @@
       }
 
       return labelArray;
+    }
+
+    function getDateDiffObj(date1, date2) {
+      const maxDate = parseDateObj(date1 >= date2 ? date1 : date2);
+      const minDate = parseDateObj(date1 < date2 ? date1 : date2);
+
+      const years = [];
+      let year = minDate.year;
+      while (year <= maxDate.year) {
+        years.push(year);
+        year++;
+      }
+
+      return years;
+    }
+
+    function parseDateObj(date) {
+      const dateView = new Date(Date.parse(date));
+      const obj = {};
+      obj.hours = dateView.getHours();
+      obj.minutes = dateView.getMinutes();
+      obj.day = dateView.getDate();
+      obj.month = dateView.getMonth() + 1;
+      obj.year = dateView.getFullYear();
+      obj.date = obj.year + '-' + (obj.month < 10 ? '0' + obj.month : obj.month) + '-' + (obj.day < 10 ? '0' + obj.day : obj.day);
+      obj.dateMonth = (obj.month < 10 ? '0' + obj.month : obj.month) + '-' + obj.year;
+      obj.dateTime = obj.date + ' ' + (obj.hours < 10 ? '0' + obj.hours : obj.hours) + ':' + (obj.minutes < 10 ? '0' + obj.minutes : obj.minutes);
+
+      return obj;
+    }
+
+    function twoDigits(n) {
+      return n < 10 ? '0' + n : n;
+    }
+
+    function getDateDiffObj(date1, date2) {
+      const maxDate = parseDateObj(date1 >= date2 ? date1 : date2);
+      const minDate = parseDateObj(date1 < date2 ? date1 : date2);
+
+      const years = [];
+      const months = [];
+      const days = [];
+      const hours = [];
+
+      let year = minDate.year;
+      while (year <= maxDate.year) {
+        years.push(year);
+        year++;
+      }
+
+      years.forEach((year) => {
+
+        let month = minDate.year >= year ? minDate.month : 1;
+
+        while ((month <= 12 && year < maxDate.year) || (month <= maxDate.month)) {
+          months.push(year + '-' + twoDigits(month));
+          month++;
+        }
+      })
+
+      months.forEach((month) => {
+        let day = minDate.dateMonth >= month ? minDate.day : 1;
+
+        const strMonth = month.substring(5, 7)
+        let nDays;
+        let m30 = ['11', '04', '06', '09'];
+        let m28 = ['02'];
+
+        if (m30.includes(strMonth)) {
+          nDays = 30;
+        } else if (m28.includes(strMonth)) {
+          if (parseInt(month.substring(0, 4)) % 4 == 0) {
+            nDays = 29
+          } else {
+            nDays = 28
+          }
+        } else {
+          nDays = 31;
+        }
+
+        while ((day <= nDays && month < maxDate.dateMonth) || (day <= maxDate.day)) {
+          days.push(month + '-' + twoDigits(day));
+          day++;
+        }
+
+      })
+
+      return {
+        years,
+        months,
+        days,
+      };
     }
   </script>
 @endpush
