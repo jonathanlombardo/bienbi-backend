b9f7090f7f80505515c60e535562e3c543786ef4
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-18 17:15:20 +0200

    init chart controller

--- app/Http/Controllers/Api/ChartController.php
+++ app/Http/Controllers/Api/ChartController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Appartment;
+use App\Models\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChartController extends Controller
 {
@@ -14,6 +17,12 @@ class ChartController extends Controller
    */
   public function datasets()
   {
-    return response()->json('test API');
+
+    $appartments = Appartment::whereBelongsTo(Auth::user())->get();
+    $appartmentsIds = $appartments->pluck('id')->toArray();
+    $views = View::whereIn('appartment_id', $appartmentsIds)->get();
+    $messages = View::whereIn('appartment_id', $appartmentsIds)->get();
+
+    return response()->json(['appartments', 'views', 'messages']);
   }
 }
